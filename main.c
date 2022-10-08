2f5dfe851694a1d553eed8c7fddc50fa9486ac9f
kirmiziben10 <76447549+kirmiziben10@users.noreply.github.com>
2022-10-09 01:27:22 +0300

    Managed to flatten 2D array to 1D
    Took me 2 or so days to  get the printToConsole() just right
    Still trying to make the printing faster
    Just might succeed

--- main.c
+++ main.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <conio.h>
 void printToConsole();
 void fillBackground(char);
 void movePlayer(char);
 void addPlayer();
-void fastPrint(char []);
+void fastPrint(char[]);
 
-#define ROWS 20
-#define COLUMNS 80
+#define ROWS 10
+#define COLUMNS 10
 
 char grid[COLUMNS][ROWS];
-char buffer[64];
+char buffer[(COLUMNS + 1) * ROWS];
 char backgroundChar = 176, playerChar = 226;
-int playerX = 0, playerY;
+int playerX = 0, playerY = 0;
 int iterations;
 
 int main()
@@ -22,18 +23,16 @@ int main()
     {
         fillBackground(backgroundChar);
         addPlayer();
-        printToConsole();
-        movePlayer(getch());
         system("cls");
+        printToConsole();
+        printf("%s", buffer);
+        movePlayer(getch());
     }
-
     return 0;
 };
 
-//Change player coordinates based on the input
 void movePlayer(char direction)
 {
-
     switch (direction)
     {
     case 'w':
@@ -49,14 +48,14 @@ void movePlayer(char direction)
         }
         break;
     case 's':
-        if (playerY != ROWS)
+        if (playerY != ROWS - 1)
         {
             playerY++;
         }
 
         break;
     case 'd':
-        if (playerX != COLUMNS)
+        if (playerX != COLUMNS - 1)
         {
             playerX++;
         }
@@ -85,16 +84,20 @@ void fillBackground(char backgroundChar)
     };
 };
 
-// Print everything to the console
+// Assign everything to the buffer
 void printToConsole()
 {
     int i, j;
+    for (int i = 0; i < (COLUMNS)*ROWS; i++)
+    {
+        buffer[i] = '0';
+    }
     for (i = 0; i < ROWS; i++)
     {
         for (j = 0; j < COLUMNS; j++)
         {
-            printf("%c", grid[j][i]);
+            buffer[i * COLUMNS + j + i] = grid[j][i];
         };
-        printf("\n");
+        buffer[(i + 1) * (COLUMNS) + i] = '\n';
     };
 };
